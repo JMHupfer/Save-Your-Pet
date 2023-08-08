3fce1cc2454a4cee4f9db11a49a53d2a82451622
JMHupfer <jmhupfer@gmail.com>
2023-08-07 20:15:57 -0600

    updated styling to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_ACCOUNT } from "../utils/queries";
@@ -10,55 +11,56 @@ const Home = () => {
 
   const petList = data?.pets || [];
 
-  let petData 
-  fetch ('https://api.fda.gov/animalandveterinary/event.json')
-  .then (data => { 
-    data.json()
-    console.log(data)
-  })
-  .then(response => {
-    console.log(response)
-  })
-  console.log(petData);
-
   return (
-    <div className="home-container">
-    <div className="home-card bg-white home-card-rounded w-50">
-      <div className="home-card-header bg-dark text-center">
-        <h1>Welcome to Save Your Pet!</h1>
-      </div>
-      <div className="home-card-body m-5">
-        <h2>Here is a list of pets you can choose from:</h2>
-        {loading ? (
-          <div>Loading...</div>
-        ) : (
-          <ul className="home-square">
-            {petList.map((pet) => {
-              return (
-                <li key={pet._id}>
-                  <Link to={{ pathname: `/pet/${pet._id}` }}>
-                    {pet.tech1} vs. {pet.tech2}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        )}
-      </div>
-      <div className="home-card-footer text-center m-3">
-        <h2>Ready to save your pet?</h2>
-        <Link to="/pet">
-          <button className="home-btn btn-lg btn-danger">Let's do it!</button>
+    <div className="container">
+      <h1>Welcome to Save Your Pet!</h1>
+
+      <div className="tabs">
+        <Link to="/adoption" className="tab">
+          Adoption
+        </Link>
+        <Link to="/medicine" className="tab">
+          Medicine
         </Link>
       </div>
-    </div>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loading ? (
+            <tr>
+              <td colSpan="2">Loading...</td>
+            </tr>
+          ) : (
+            petList.map((pet) => (
+              <tr key={pet._id}>
+                <td>{pet.name}</td>
+                <td>
+                  <Link to={`/pet/${pet._id}`}>
+                    {pet.tech1} vs. {pet.tech2}
+                  </Link>
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </table>
+
+      <div className="footer">
+        <h2>Ready to save your pet?</h2>
+        <Link to="/pet">
+          <button className="btn btn-danger">Let's do it!</button>
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Home;
 
-
-
 // api URL https://api.fda.gov/animalandveterinary/event.json
-
