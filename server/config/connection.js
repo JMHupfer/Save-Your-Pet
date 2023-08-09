517a0a95fb31160ebeecf45a807be9d25f3e74e9
JMHupfer <jmhupfer@gmail.com>
2023-08-08 19:07:50 -0600

    added atlas key and bird to adoption seed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/pet_DB',
-    {
-        useNewUrlParser:true,
-        useUnifiedTopology: true,
-    }
+  process.env.MONGODB_URI ||
+    "mongodb+srv://jmhupfer:Germanboy_321@cluster0.tmenay1.mongodb.net/pet_DB?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
-module.exports = mongoose.connection
+module.exports = mongoose.connection;
